5edfe4dafd6f6cf795fd0cf50fd8bc1832b1d9cb
Andrei Oprescu <A.Oprescu-1@student.tudelft.nl>
2024-03-07 16:52:15 +0100

    deleted config json and made the first query.

--- src/main/java/tudelft/wis/idm_tasks/basicJDBC/classes/Retriever.java
+++ src/main/java/tudelft/wis/idm_tasks/basicJDBC/classes/Retriever.java
@@ -1,18 +1,50 @@
+package tudelft.wis.idm_tasks.basicJDBC.classes;
+
+import tudelft.wis.idm_tasks.basicJDBC.interfaces.JDBCTask2Interface;
+
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 class Retriever implements JDBCTask2Interface {
+    @Override
+    public Connection getConnection() {
+        return new Retriever().getConnection();
+    }
+
     public Collection<String> getTitlesPerYear(int year) {
-        Connection conn = JDBCManager.getConnection();
-        conn.setAutoCommit(false);
-        PreparedStatement changeNameStmt = conn.prepareStatement(
-                "UPDATE hero SET name=? WHERE name=?"
-        );
-        changeNameStmt.setString(1, "Jean Grey-Summers");
-        changeNameStmt.setString(2, "Jean Grey");
-        changeNameStmt.executeUpdate();
+        Connection conn = getConnection();
+        try {
+            conn.setAutoCommit(false);
+            PreparedStatement titlesQuery = conn.prepareStatement(
+                    "SELECT t.primary_title FROM titles t WHERE t.start_year = ?"
+            );
+            titlesQuery.setInt(1, year);
+            ResultSet result = titlesQuery.executeQuery();
+            List<String> resultList = new ArrayList<>();
+            while(result.next()) {
+                resultList.add(result.getString("primary_title"));
+            }
+            return resultList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        changeNameStmt.setString(1, "Scott Grey-Summers");
-        changeNameStmt.setString (2, "Scott Summers");
-        changeNameStmt.executeUpdate();
+    @Override
+    public Collection<String> getJobCategoriesFromTitles(String searchString) {
+        return null;
+    }
 
-        conn.commit();
+    @Override
+    public double getAverageRuntimeOfGenre(String genre) {
+        return 0;
+    }
+
+    @Override
+    public Collection<String> getPlayedCharacters(String actorFullname) {
+        return null;
     }
 }
