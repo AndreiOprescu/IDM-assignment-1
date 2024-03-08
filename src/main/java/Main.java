745ed4faa4cfec50a75ffc25bb35f04825714b02
Mihai-Filip Franculescu <M.F.Franculescu@student.tudelft.nl>
2024-03-08 20:32:29 +0100

    Finishing query 3 and query 4 and updating Main to print all the first 20 results of the queries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,25 @@ public class Main {
         Retriever retriever = new Retriever();
 
         Collection<String> query1 = retriever.getTitlesPerYear(2019);
-        System.out.println(query1);
+        System.out.println("Query 1:");
+        System.out.println(query1.stream()
+                .limit(20)
+                .toList());
 
         Collection<String> query2 = retriever.getJobCategoriesFromTitles("batman");
-        System.out.println(query2);
+        System.out.println("Query 2:");
+        System.out.println(query2.stream()
+                .limit(20)
+                .toList());
+        
+        double query3 = retriever.getAverageRuntimeOfGenre("Comedy");
+        System.out.println("Query 3:");
+        System.out.println(query3);
+        
+        Collection<String> query4 = retriever.getPlayedCharacters("Robert Downey Jr.");
+        System.out.println("Query 4:");
+        System.out.println(query4.stream()
+                .limit(20)
+                .toList());
     }
 }
--- src/main/java/tudelft/wis/idm_tasks/basicJDBC/classes/Retriever.java
+++ src/main/java/tudelft/wis/idm_tasks/basicJDBC/classes/Retriever.java
@@ -13,24 +13,19 @@ public class Retriever implements JDBCTask2Interface {
     public Connection getConnection() {
         try {
             return new Manager().getConnection();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (FileNotFoundException e) {
+        } catch (SQLException | ClassNotFoundException | FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-
+    
     public Collection<String> getTitlesPerYear(int year) {
         Connection conn = getConnection();
         try {
             conn.setAutoCommit(false);
             PreparedStatement titlesQuery = conn.prepareStatement(
                     "SELECT t.primary_title " +
-                    "FROM titles t " +
-                    "WHERE t.start_year = ? " +
-                    "LIMIT 20"
+                            "FROM titles t " +
+                            "WHERE t.start_year = ? "
             );
             titlesQuery.setInt(1, year);
             ResultSet result = titlesQuery.executeQuery();
@@ -43,7 +38,7 @@ public class Retriever implements JDBCTask2Interface {
             throw new RuntimeException(e);
         }
     }
-
+    
     @Override
     public Collection<String> getJobCategoriesFromTitles(String searchString) {
         Connection conn = getConnection();
@@ -51,10 +46,9 @@ public class Retriever implements JDBCTask2Interface {
             conn.setAutoCommit(false);
             PreparedStatement titlesQuery = conn.prepareStatement(
                     "SELECT DISTINCT ci.job_category " +
-                    "FROM titles t " +
-                    "JOIN cast_info ci ON ci.title_id = t.title_id " +
-                    "WHERE t.primary_title LIKE ? " +
-                    "LIMIT 20"
+                            "FROM titles t " +
+                            "JOIN cast_info ci ON ci.title_id = t.title_id " +
+                            "WHERE t.primary_title LIKE ? "
             );
             searchString = "%" + searchString + "%";
             titlesQuery.setString(1, searchString);
@@ -68,14 +62,45 @@ public class Retriever implements JDBCTask2Interface {
             throw new RuntimeException(e);
         }
     }
-
+    
     @Override
     public double getAverageRuntimeOfGenre(String genre) {
-        return 0;
+        Connection conn = getConnection();
+		try {
+			conn.setAutoCommit(false);
+			PreparedStatement query = conn.prepareStatement(
+					"SELECT AVG(t.runtime) AS average FROM titles_genres tg\n" +
+						"JOIN titles t ON tg.title_id=t.title_id\n" +
+						"WHERE tg.genre=?\n" +
+						"GROUP BY tg.genre"
+			);
+			query.setString(1, genre);
+			ResultSet result = query.executeQuery();
+			if (result.next()) return result.getDouble("average");
+			return -1;
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
     }
-
+    
     @Override
-    public Collection<String> getPlayedCharacters(String actorFullname) {
-        return null;
+    public Collection<String> getPlayedCharacters(String actorFullName) {
+		Connection conn = getConnection();
+        try {
+            conn.setAutoCommit(false);
+            PreparedStatement query = conn.prepareStatement(
+			"SELECT DISTINCT character_name FROM title_person_character tpc\n" +
+				"WHERE tpc.person_id=(SELECT person_id FROM persons p WHERE full_name=?)"
+            );
+            query.setString(1, actorFullName);
+            ResultSet result = query.executeQuery();
+            List<String> resultList = new ArrayList<>();
+            while(result.next()) {
+                resultList.add(result.getString("character_name"));
+            }
+            return resultList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
